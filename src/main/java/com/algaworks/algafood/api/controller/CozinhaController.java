8d5a3a9156640d0567a4fc8b59f91bb676766797
João Pedro <jpedrodaibello1999@outlook.com>
2022-04-07 08:40:05 -0300

    feat: fixing HTTP status for non-existent resource

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -27,7 +27,12 @@ public class CozinhaController {
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-		// return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-		return ResponseEntity.ok(cozinha);
+
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+
+		// return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		return ResponseEntity.notFound().build();
 	}
 }
