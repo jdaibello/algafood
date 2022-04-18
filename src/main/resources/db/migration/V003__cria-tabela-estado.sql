c348f51f0fd971ed88634ced9a8ddfa8323840b5
João Pedro <jpedrodaibello1999@outlook.com>
2022-04-18 15:13:51 -0300

    feat: creating complex migrations with data relocation

---
+++ src/main/resources/db/migration/V003__cria-tabela-estado.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Estado (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(40) NOT NULL,
+	PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO Estado (nome) SELECT DISTINCT nome_estado FROM Cidade;
+
+ALTER TABLE Cidade ADD COLUMN estado_id BIGINT NOT NULL;
+
+UPDATE Cidade c SET c.estado_id = (SELECT e.id FROM Estado e WHERE e.nome = c.nome_estado);
+
+ALTER TABLE Cidade ADD CONSTRAINT fk_cidade_estado FOREIGN KEY (estado_id) REFERENCES Estado (id);
+
+ALTER TABLE Cidade DROP COLUMN nome_estado;
+ALTER TABLE Cidade CHANGE nome_cidade nome VARCHAR(80) NOT NULL;
