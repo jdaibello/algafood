fdfdb8c96ba7c995b52b9f2f2aa5b1c250ef69f7
João Pedro <jpedrodaibello1999@outlook.com>
2022-04-18 22:25:08 -0300

    feat: simplifying the code using @ResponseStatus on exceptions

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
@@ -12,6 +12,9 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @Service
 public class CadastroCozinhaService {
+	private static final String MSG_COZINHA_EM_USO = "Cozinha de código %d não pode ser removida, pois está em uso";
+	private static final String MSG_COZINHA_NAO_ENCONTRADA = "Não existe uma cozinha com o código %d";
+
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 
@@ -24,12 +27,15 @@ public class CadastroCozinhaService {
 			cozinhaRepository.deleteById(cozinhaId);
 
 		} catch (EmptyResultDataAccessException e) {
-			throw new EntidadeNaoEncontradaException(
-					String.format("Não existe uma cozinha com o código %d", cozinhaId));
+			throw new EntidadeNaoEncontradaException(String.format(MSG_COZINHA_NAO_ENCONTRADA, cozinhaId));
 
 		} catch (DataIntegrityViolationException e) {
-			throw new EntidadeEmUsoException(
-					String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
+			throw new EntidadeEmUsoException(String.format(MSG_COZINHA_EM_USO, cozinhaId));
 		}
+	}
+
+	public Cozinha buscarOuFalhar(Long cozinhaId) {
+		return cozinhaRepository.findById(cozinhaId).orElseThrow(
+				() -> new EntidadeNaoEncontradaException(String.format(MSG_COZINHA_NAO_ENCONTRADA, cozinhaId)));
 	}
 }
