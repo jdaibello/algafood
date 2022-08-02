88d792b5f6e3df1d35977aa52ca9b23b2770e892
João Pedro <jpedrodaibello1999@outlook.com>
2022-08-01 21:26:54 -0300

    feat: doing resource projection with @JsonView from Jackson

--- src/main/java/com/algaworks/algafood/api/controller/RestaurantController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestaurantController.java
@@ -1,25 +1,10 @@
 package com.algaworks.algafood.api.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.algaworks.algafood.api.assembler.RestaurantDTOAssembler;
 import com.algaworks.algafood.api.assembler.RestaurantInputDisassembler;
 import com.algaworks.algafood.api.dto.RestaurantDTO;
 import com.algaworks.algafood.api.dto.input.RestaurantInput;
+import com.algaworks.algafood.api.dto.view.RestaurantView;
 import com.algaworks.algafood.domain.exception.BusinessException;
 import com.algaworks.algafood.domain.exception.CityNotFoundException;
 import com.algaworks.algafood.domain.exception.KitchenNotFoundException;
@@ -27,6 +12,13 @@ import com.algaworks.algafood.domain.exception.RestaurantNotFoundException;
 import com.algaworks.algafood.domain.model.Restaurant;
 import com.algaworks.algafood.domain.repository.RestaurantRepository;
 import com.algaworks.algafood.domain.service.RestaurantService;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/restaurants")
@@ -44,6 +36,7 @@ public class RestaurantController {
 	@Autowired
 	private RestaurantInputDisassembler restaurantInputDisassembler;
 
+	@JsonView(RestaurantView.Summary.class)
 	@GetMapping
 	public List<RestaurantDTO> fetchAll() {
 		return restaurantDTOAssembler.toCollectionModel(restaurantRepository.findAll());
--- src/main/java/com/algaworks/algafood/api/dto/KitchenDTO.java
+++ src/main/java/com/algaworks/algafood/api/dto/KitchenDTO.java
@@ -1,11 +1,17 @@
 package com.algaworks.algafood.api.dto;
 
+import com.algaworks.algafood.api.dto.view.RestaurantView;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class KitchenDTO {
+
+	@JsonView(RestaurantView.Summary.class)
 	private Long id;
+
+	@JsonView(RestaurantView.Summary.class)
 	private String name;
 }
