22ddef71eacb4d586da93be4591f8a9e51ddfc38
João Pedro <jpedrodaibello1999@outlook.com>
2022-08-04 12:03:23 -0300

    feat: validating maximum file size

--- src/main/java/com/algaworks/algafood/api/controller/RestaurantProductPhotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestaurantProductPhotoController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,7 +20,7 @@ public class RestaurantProductPhotoController {
 
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public void updatePhoto(@PathVariable Long restaurantId, @PathVariable Long productId,
-			ProductPhotoInput productPhotoInput) {
+			@Valid ProductPhotoInput productPhotoInput) {
 		var fileName = UUID.randomUUID().toString() + "_" + productPhotoInput.getFile().getOriginalFilename();
 		var photoFile = Path.of("C:\\dev\\java\\catalog", fileName);
 
--- src/main/java/com/algaworks/algafood/api/dto/input/ProductPhotoInput.java
+++ src/main/java/com/algaworks/algafood/api/dto/input/ProductPhotoInput.java
@@ -1,6 +1,11 @@
 package com.algaworks.algafood.api.dto.input;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.algafood.core.validation.FileSize;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +13,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ProductPhotoInput {
+
+	@NotNull
+	@FileSize(max = "5MB")
 	private MultipartFile file;
+
+	@NotBlank
 	private String description;
 }
