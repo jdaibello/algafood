91dea0a3553529db057e5e813980da2fb9ebe39a
João Pedro <jpedrodaibello1999@outlook.com>
2022-08-08 21:51:50 -0300

    feat: challenge - implementing a GET request with JavaScript

--- client-js/client.js
+++ client-js/client.js
@@ -1,11 +1,26 @@
-function findRestaurants() {
+function query() {
 	$.ajax({
-		url: "http://localhost:8080/restaurants",
+		url: "http://localhost:8080/payment-methods",
 		type: "GET",
 		success: function (response) {
-			$("#content").text(JSON.stringify(response));
+			fillTable(response);
 		}
 	});
 }
 
-$("#button").click(findRestaurants);
+function fillTable(paymentMethods) {
+	$("#table tbody tr").remove();
+
+	$.each(paymentMethods, function (i, paymentMethod) {
+		var row = $("<tr>");
+
+		row.append(
+			$("<td>").text(paymentMethod.id),
+			$("<td>").text(paymentMethod.description)
+		);
+
+		row.appendTo("#table");
+	});
+}
+
+$("#btn-query").click(query);
