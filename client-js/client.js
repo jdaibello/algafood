209b5e91b7227ee229994c75aa5fc82de5421bca
João Pedro <jpedrodaibello1999@outlook.com>
2022-08-08 22:13:24 -0300

    feat: implementing a registration form and making a POST request with JavaScript

--- client-js/client.js
+++ client-js/client.js
@@ -1,9 +1,34 @@
-function query() {
+function consult() {
 	$.ajax({
 		url: "http://localhost:8080/payment-methods",
 		type: "GET",
 		success: function (response) {
 			fillTable(response);
+		}
+	});
+}
+
+function register() {
+	var paymentMethodJson = JSON.stringify({
+		"description": $("#description-field").val(),
+	});
+
+	$.ajax({
+		url: "http://localhost:8080/payment-methods",
+		type: "POST",
+		data: paymentMethodJson,
+		contentType: "application/json",
+		success: function (response) {
+			alert("Forma de pagamento adicionada!");
+			consult();
+		},
+		error: function (error) {
+			if (error.status === 400) {
+				var problem = JSON.parse(error.responseText);
+				alert(problem.userMessage);
+			} else {
+				alert("Erro ao cadastrar forma de pagamento!");
+			}
 		}
 	});
 }
@@ -23,4 +48,5 @@ function fillTable(paymentMethods) {
 	});
 }
 
-$("#btn-query").click(query);
+$("#btn-consult").click(consult);
+$("#btn-register").click(register);
