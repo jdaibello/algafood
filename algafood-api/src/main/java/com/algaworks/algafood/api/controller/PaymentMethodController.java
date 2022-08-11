6ff33785a374bc3103ff9be0fde1a6619eac27dc
João Pedro <jpedrodaibello1999@outlook.com>
2022-08-11 09:20:56 -0300

    feat: implementing conditional requests with Deep ETags

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/PaymentMethodController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/PaymentMethodController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.controller;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import com.algaworks.algafood.api.assembler.PaymentMethodDTOAssembler;
 import com.algaworks.algafood.api.assembler.PaymentMethodInputDisassembler;
@@ -44,11 +47,24 @@ public class PaymentMethodController {
 	private PaymentMethodInputDisassembler paymentMethodInputDisassembler;
 
 	@GetMapping
-	public ResponseEntity<List<PaymentMethodDTO>> fetchAll() {
+	public ResponseEntity<List<PaymentMethodDTO>> fetchAll(ServletWebRequest request) {
+		ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+
+		String etag = "0";
+		OffsetDateTime lastUpdateDate = paymentMethodRepository.getLastUpdateDate();
+
+		if (lastUpdateDate != null) {
+			etag = String.valueOf(lastUpdateDate.toEpochSecond());
+		}
+
+		if (request.checkNotModified(etag)) {
+			return null;
+		}
+
 		List<PaymentMethodDTO> paymentMethodsDTO = paymentMethodDTOAssembler
 				.toCollectionModel(paymentMethodRepository.findAll());
 
-		return ResponseEntity.ok().cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+		return ResponseEntity.ok().cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic()).eTag(etag)
 				.body(paymentMethodsDTO);
 	}
 
