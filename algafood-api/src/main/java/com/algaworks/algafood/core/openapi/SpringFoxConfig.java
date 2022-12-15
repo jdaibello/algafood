345a776e7ca07da98a50e3a0eab27b7e343c902a
João Pedro <jpedrodaibello1999@outlook.com>
2022-12-15 08:30:31 -0300

    feat: describing response status codes globally

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,15 +1,22 @@
 package com.algaworks.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -21,8 +28,10 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 	@Bean
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.OAS_30).select()
-				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api")).build().apiInfo(apiInfo())
-				.tags(new Tag("Cidades", "Gerencia as cidades")).tags(new Tag("Grupos", "Gerencia os grupos"))
+				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api")).build()
+				.useDefaultResponseMessages(false).globalResponses(HttpMethod.GET, globalGetResponseMessages())
+				.apiInfo(apiInfo()).tags(new Tag("Cidades", "Gerencia as cidades"))
+				.tags(new Tag("Grupos", "Gerencia os grupos"))
 				.tags(new Tag("Permissões dos Grupos", "Gerencia as permissões dos grupos"))
 				.tags(new Tag("Cozinhas", "Gerencia as cozinhas")).tags(new Tag("Pedidos", "Gerencia os pedidos"))
 				.tags(new Tag("Fluxos dos Pedidos", "Gerencia os fluxos (status) dos pedidos"))
@@ -40,7 +49,15 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.tags(new Tag("Grupos de Usuários", "Gerencia os grupos de usuários"));
 	}
 
-	public ApiInfo apiInfo() {
+	private List<Response> globalGetResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+						.description("Erro interno do servidor").build(),
+				new ResponseBuilder().code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+						.description("Recurso não possui representação que pode ser aceita pelo consumidor").build());
+	}
+
+	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder().title("Algafood API").description("API aberta para clientes e restaurantes")
 				.version("1.0.0").contact(new Contact("João Pedro", "https://github.com/jdaibello/algafood", ""))
 				.build();
