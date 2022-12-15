8d7544db801c2bfcb66a769c46ed81ac05b6a325
João Pedro <jpedrodaibello1999@outlook.com>
2022-12-15 15:11:35 -0300

    feat: referencing Problem representation model with error status codes

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -2,12 +2,14 @@ package com.algaworks.algafood.core.openapi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
@@ -16,6 +18,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -65,9 +68,9 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 	}
 
 	private List<Response> globalGetResponseMessages() {
-		return Arrays.asList(
-				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-						.description("Erro interno do servidor").build(),
+		return Arrays.asList(new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+				.description("Erro interno do servidor").representation(MediaType.APPLICATION_JSON)
+				.apply(getProblemModelReference()).build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
 						.description("Recurso não possui representação que pode ser aceita pelo consumidor").build());
 	}
@@ -75,21 +78,31 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 	private List<Response> globalPostPutResponseMessages() {
 		return Arrays.asList(
 				new ResponseBuilder().code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-						.description("Requisição inválida - erro do cliente").build(),
+						.description("Requisição inválida - erro do cliente").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemModelReference()).build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-						.description("Erro interno do servidor").build(),
+						.description("Erro interno do servidor").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemModelReference()).build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
 						.description("Recurso não possui representação que pode ser aceita pelo consumidor").build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
-						.description("Requisição recusada porque o corpo está em um formato não suportado").build());
+						.description("Requisição recusada porque o corpo está em um formato não suportado")
+						.representation(MediaType.APPLICATION_JSON).apply(getProblemModelReference()).build());
 	}
 
 	private List<Response> globalDeleteResponseMessages() {
 		return Arrays.asList(
 				new ResponseBuilder().code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-						.description("Requisição inválida - erro do cliente").build(),
+						.description("Requisição inválida - erro do cliente").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemModelReference()).build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-						.description("Erro interno do servidor").build());
+						.description("Erro interno do servidor").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemModelReference()).build());
+	}
+
+	private Consumer<RepresentationBuilder> getProblemModelReference() {
+		return r -> r.model(m -> m.name("Problem").referenceModel(ref -> ref.key(k -> k
+				.qualifiedModelName(q -> q.name("Problem").namespace("com.algaworks.algafood.api.exceptionhandler")))));
 	}
 
 	private ApiInfo apiInfo() {
