240479a81eee37b1889399c3ec5e071fb58fd724
João Pedro <jpedrodaibello1999@outlook.com>
2023-08-10 14:07:21 -0300

    feat: configuring OAuth2 clients on an SQL database

--- algafood-auth/src/main/java/com/algaworks/algafood/auth/core/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/core/AuthorizationServerConfig.java
@@ -21,14 +21,12 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
+import javax.sql.DataSource;
 import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private AuthenticationConfiguration authenticationConfiguration;
@@ -39,19 +37,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private JwtKeyStoreProperties jwtKeyStoreProperties;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("algafood-mobile").secret(passwordEncoder.encode("mobile123"))
-                .authorizedGrantTypes("password", "refresh_token").scopes("WRITE", "READ")
-                .accessTokenValiditySeconds(6 * 60 * 60).refreshTokenValiditySeconds(60 * 24 * 60 * 60)
-                .and().withClient("food_analytics").secret(passwordEncoder.encode("food123"))
-                .authorizedGrantTypes("authorization_code", "refresh_token").scopes("WRITE", "READ")
-                .redirectUris("http://client-application")
-                .and().withClient("webadmin").authorizedGrantTypes("implicit").scopes("WRITE", "READ")
-                .redirectUris("http://client-application")
-                .and().withClient("billing").secret(passwordEncoder.encode("billing123"))
-                .authorizedGrantTypes("client_credentials").scopes("WRITE", "READ")
-                .and().withClient("checktoken").secret(passwordEncoder.encode("check123"));
+        clients.jdbc(dataSource);
     }
 
     @Override
